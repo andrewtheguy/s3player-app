db4e836dced74633b7f67de99c84b537eb147596
Andrew Chen <andrew@jaanuu.com>
2026-05-01 15:03:04 -0700

    Remove S3 configuration and object service along with associated tests and smoke test

--- Package.swift
+++ Package.swift
@@ -8,39 +8,7 @@ let package = Package(
         .iOS(.v17),
         .macOS(.v14)
     ],
-    products: [
-        .library(
-            name: "S3PlayerKit",
-            targets: ["S3PlayerKit"]
-        ),
-        .executable(
-            name: "s3-smoke-test",
-            targets: ["S3SmokeTest"]
-        )
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/awslabs/aws-sdk-swift",
-            from: "1.6.106"
-        )
-    ],
-    targets: [
-        .target(
-            name: "S3PlayerKit",
-            dependencies: [
-                .product(name: "AWSS3", package: "aws-sdk-swift"),
-                .product(name: "AWSSDKIdentity", package: "aws-sdk-swift")
-            ]
-        ),
-        .executableTarget(
-            name: "S3SmokeTest",
-            dependencies: ["S3PlayerKit"],
-            path: "Sources/S3SmokeTest"
-        ),
-        .testTarget(
-            name: "S3PlayerKitTests",
-            dependencies: ["S3PlayerKit"],
-            path: "Tests/S3PlayerKitTests"
-        )
-    ]
+    products: [],
+    dependencies: [],
+    targets: []
 )
